be19e4bdde15c04ee9ee8739f9dcfb481b60786e
Raghad Akila <raghadakila@gmail.com>
2026-03-29 15:37:35 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartFurnitureStoreSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
